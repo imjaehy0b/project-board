ecd19dd5085581ec6c9e0623209216f494405739
imjae <imjae950@gmail.com>
2024-01-16 18:01:19 +0900

    #21 - 게시판 컨트롤러 구현
    게시판 기본 조회 화면을 위한 컨트롤러 구현하고 , 데이터 전달 완료
    게시판 페이지 기본 정렬 속성 부여 : ( 10개 1페이지, 시간 내림차순)

--- src/test/java/com/project/board/controller/ArticleControllerTest.java
+++ src/test/java/com/project/board/controller/ArticleControllerTest.java
@@ -1,5 +1,9 @@
 package com.project.board.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
@@ -7,12 +11,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import com.project.board.config.SecurityConfig;
+import com.project.board.dto.ArticleWithCommentsDto;
+import com.project.board.dto.UserAccountDto;
+import com.project.board.service.ArticleService;
+import java.time.LocalDateTime;
+import java.util.Set;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,6 +35,8 @@ class ArticleControllerTest {
 
     private final MockMvc mvc;
 
+    @MockBean
+    private ArticleService articleService;
 
     public ArticleControllerTest(@Autowired MockMvc mvc) {//테스트 영역은 autowired 생략 불가.
         this.mvc = mvc;
@@ -32,6 +46,7 @@ class ArticleControllerTest {
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnArticlesView() throws Exception {
         //given
+        given(articleService.searchArticles(eq(null),eq(null),any(Pageable.class))).willReturn(Page.empty());
 
         //when&then
         mvc.perform(get("/articles"))
@@ -39,12 +54,15 @@ class ArticleControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML)) //타입검사
                 .andExpect(view().name("articles/index")) //뷰 체크
                 .andExpect(model().attributeExists("articles")); //데이터가 존재하는지 체크
+        then(articleService).should().searchArticles(eq(null),eq(null),any(Pageable.class));
     }
 
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnArticleView() throws Exception {
         //given
+        Long articleId = 1L;
+        given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
 
         //when&then
         mvc.perform(get("/articles/1"))
@@ -53,10 +71,12 @@ class ArticleControllerTest {
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article")) //데이터 검사
                 .andExpect(model().attributeExists("articleComments"));
+        then(articleService).should().getArticle(articleId);
     }
 
+
     @Disabled("구현중")
-    @DisplayName("[view][GET] 게시글 검색 페이지 - 정상 호출")
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingArticleSearchView_thenReturnArticleSearchView() throws Exception {
         //given
@@ -79,5 +99,35 @@ class ArticleControllerTest {
                 .andExpect(status().isOk()) //상태 검사
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML)) //타입검사
                 .andExpect(view().name("articles/search-hashtag"));
+    }
+
+
+    private ArticleWithCommentsDto createArticleWithCommentsDto() {
+        return ArticleWithCommentsDto.of(
+                1L,
+                createUserAccountDto(),
+                Set.of(),
+                "title",
+                "content",
+                "#java",
+                LocalDateTime.now(),
+                "uno",
+                LocalDateTime.now(),
+                "uno"
+        );
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(1L,
+                "uno",
+                "pw",
+                "uno@mail.com",
+                "Uno",
+                "memo",
+                LocalDateTime.now(),
+                "uno",
+                LocalDateTime.now(),
+                "uno"
+        );
     }
 }
